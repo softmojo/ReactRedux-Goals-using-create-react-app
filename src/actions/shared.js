07cadfd7c6079c285aa91aba69eb26f6c58df469
Abdulmajid Mubarak <softrmojo@gmail.com>
2020-05-08 23:38:16 +0100

    Actions

---
+++ src/actions/shared.js
@@ -0,0 +1,25 @@
+import API from 'goals-todos-api'
+
+export const RECEIVE_DATA = 'RECEIVE_DATA'
+
+function receiveDataAction(todos, goals) {
+    return {
+        type: RECEIVE_DATA,
+        todos,
+        goals,
+    }
+}
+
+
+
+function handleInitialData() {
+    return (dispatch) => {
+        return Promise.all([
+            API.fetchTodos(),
+            API.fetchGoals()
+        ]).then(([todos, goals]) => {
+            dispatch(receiveDataAction(todos, goals))
+        })
+    }
+}
+
