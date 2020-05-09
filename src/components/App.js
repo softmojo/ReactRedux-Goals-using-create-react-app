a8a340274920abb4deb22aed00a8386ed326e4fd
Abdulmajid Mubarak <softrmojo@gmail.com>
2020-05-09 12:08:03 +0100

    Components

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,46 @@
-import React from 'react';
 
-function App() {
-    return (
-        <div>
-            Hello World
-        </div>
-    );
+class App extends React.Component {
+
+    componentDidMount() {
+        const { dispatch } = this.props
+        dispatch(handleInitialData())
+    }
+
+    render() {
+        if (this.props.loading === true) {
+            return <h3>Loading</h3>
+        }
+
+        return (
+            <div>
+                <ConnectedTodos />
+                <ConnectedGoals />
+            </div>
+        )
+    }
 }
 
-export default App;
+class App extends React.Component {
+
+    componentDidMount() {
+        const { dispatch } = this.props
+        dispatch(handleInitialData())
+    }
+
+    render() {
+        if (this.props.loading === true) {
+            return <h3>Loading</h3>
+        }
+
+        return (
+            <div>
+                <ConnectedTodos />
+                <ConnectedGoals />
+            </div>
+        )
+    }
+}
+
+export default connect((state) => ({
+    loading: state.loading
+}))(App)
